e2b64d475d6419faf3ef349112572a6b4332ff13
m4rkos <marcos.j.eduardo@gmail.com>
2018-01-02 12:14:29 -0200

    Commit atualização, inclusão do bootstrap | 02/01/2018

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="container">
+        <div></div>
+        <div></div>
+    </div>
+    <div class="container">
+        <div class="col-md-8">copyright casa xiq - <?php echo date('Y');?> | www.casaxiq.com.br, created by MOOW</div>
+        <div class="col-md-4"><?php include_once"./includes/social-net.php";?></div>
+    </div>
+</div>
